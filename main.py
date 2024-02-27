795319e237454ab8519c4ba96a6fe81657df9981
seohyunjun <bnmy6581@gmail.com>
2024-02-28 08:34:13 +0900

    CI

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import imaplib
 import os
 
 import json
-        
+import re
+            
 from dateutil.parser import parse
 import datetime
 
 import email
 from email.header import decode_header, make_header
 
+import google.generativeai as genai
 
 load_dotenv()
 
@@ -19,7 +21,19 @@ PASSWORD = os.getenv('PASSWORD')
 IMAP4_SSL = os.getenv('IMAP4_SSL')
 PORT = os.getenv('PORT')
 LABEL = os.getenv('LABEL')
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+def genai_transform(text):
+    try:
+        model = genai.GenerativeModel('gemini-pro')
+        prompt = "Summarize the following text: " + text
+        response = model.generate_content(prompt)
+        text = response.text
+        return text
+    except:
+        return text
 
 # INBOX 메일함 선택
 def check_mailbox(server, mailbox):
@@ -50,11 +64,19 @@ def get_mail(no):
             #     body = part.get_payload(decode=True)
             #     break
     else:
-        pass
-        #body = message.get_payload(decode=True)
+        body = message.get_payload(decode=True)
         
-    #body = body.decode('utf-8')    
-    return f"{date.__str__()} {fr.__str__()} {subject.__str__()}"
+    body = body.decode('utf-8') 
+    
+    SendAddress_regex = r'[\w\.-]+@[\w\.-]+'
+    SendAddress = re.findall(SendAddress_regex, fr.__str__())
+    if SendAddress:
+        fr = SendAddress[0]
+    else:
+        fr = fr.__str__() 
+    message = genai_transform(f"{subject.__str__()} {body}")
+    return  f"{date.__str__()} {fr} {message}"
+    
 if __name__=='__main__':
     
     
@@ -81,8 +103,10 @@ if __name__=='__main__':
         
         message = ""
         for num in data[0].split():
-            fields.append({"type": "mrkdwn","text": f"{get_mail(num)}"})
-            
+            mail_text = get_mail(num)
+            print(mail_text)
+            fields.append({"type": "mrkdwn","text": f"{mail_text}"})
+            fields.append({"type": "divider"})
         temp = {
               "text": "Unread Email",
               "blocks": [
