6eb87358450dc5c2e8d7686d75a356fc589d96b4
seohyunjun <bnmy6581@gmail.com>
2024-03-03 13:00:23 +0900

    d

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import html2text
 
 import json
 import re
-            
+
 from dateutil.parser import parse
 import datetime
 
@@ -20,56 +20,62 @@ import google.generativeai as genai
 
 load_dotenv()
 
-EMAIL = os.getenv('EMAIL')
-PASSWORD = os.getenv('PASSWORD')
-IMAP4_SSL = os.getenv('IMAP4_SSL')
-PORT = os.getenv('PORT')
-LABEL = os.getenv('LABEL')
-GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+IMAP4_SSL = os.getenv("IMAP4_SSL")
+PORT = os.getenv("PORT")
+LABEL = os.getenv("LABEL")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
 
 def clean(text):
     return "".join(c if str(c).isalnum() else "_" for c in text)
 
-def genai_transform(text: str, genai: genai, prompt: str="Summarize 20 words the following text: "):
-    model = genai.GenerativeModel('gemini-pro')
+
+def genai_transform(
+    text: str, genai: genai, prompt: str = "Summarize 20 words the following text: "
+):
+    model = genai.GenerativeModel("gemini-pro")
     prompt = prompt + text
     response = model.generate_content(prompt)
     text = response.text
     # slack block message max length is 255.
     return text[:210]
 
+
 # INBOX 메일함 선택
 def check_mailbox(server, mailbox):
     state, boxes = server.list()
-    if state=="OK":
-        if LABEL in [box.decode().split(' "/" ')[1].replace("\"","") for box in boxes]:
-            print('Mailbox found')
+    if state == "OK":
+        if LABEL in [box.decode().split(' "/" ')[1].replace('"', "") for box in boxes]:
+            print("Mailbox found")
             return "Mailbox found"
         else:
             return "Can't find the mailbox"
-    
+
     else:
         return "Can't connect to the mailbox"
 
+
 def get_mail(num, genai):
-    result, email_data = server.uid('fetch',num, '(RFC822)')
+    result, email_data = server.uid("fetch", num, "(RFC822)")
     raw_email = email_data[0][1]
     message = email.message_from_bytes(raw_email)
-    raw_email_str = raw_email.decode('utf-8')
+    raw_email_str = raw_email.decode("utf-8")
 
-    date = make_header(decode_header(message.get('Date')))
-    fr = make_header(decode_header(message.get('From')))
-    subject = make_header(decode_header(message.get('Subject')))
+    date = make_header(decode_header(message.get("Date")))
+    fr = make_header(decode_header(message.get("From")))
+    subject = make_header(decode_header(message.get("Subject")))
     email_message = email.message_from_string(raw_email_str)
     for part in email_message.walk():
         print(part.get_content_type())
         if part.get_content_type() == "text/plain":
             body = part.get_payload(decode=True)
-            body = body.decode("utf-8")             # Convert byte to str
+            body = body.decode("utf-8")  # Convert byte to str
             body = body.replace("\r\n", " ")
-            text = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', body)
-            text2 = text.translate(str.maketrans('', '', string.punctuation))
-            body_list = re.sub("[^\\w]", " ",  text2).split()
+            text = re.sub(r"\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*", "", body)
+            text2 = text.translate(str.maketrans("", "", string.punctuation))
+            body_list = re.sub("[^\\w]", " ", text2).split()
         elif part.get_content_type() == "text/html":
             # body = body.decode("utf-8")             # Convert byte to str
             body = html2text.html2text(part.__str__())
@@ -77,67 +83,68 @@ def get_mail(num, genai):
             # html = open("foobar.html").read()
         else:
             continue
-    SendAddress_regex = r'[\w\.-]+@[\w\.-]+'
+    SendAddress_regex = r"[\w\.-]+@[\w\.-]+"
     SendAddress = re.findall(SendAddress_regex, fr.__str__())
     if SendAddress:
         fr = SendAddress[0]
     else:
-        fr = fr.__str__() 
-        
+        fr = fr.__str__()
+
     message = genai_transform(f"""{subject.__str__()} {body}""", genai)
-    return  f"{date.__str__()} {fr} {message}"
+    return f"{date.__str__()} {fr} {message}"
 
 
-if __name__=='__main__':
-    genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
-    
+if __name__ == "__main__":
+    genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
     server = imaplib.IMAP4_SSL(IMAP4_SSL, port=PORT)
     server.login(EMAIL, PASSWORD)
 
-    since = datetime.datetime.date(datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%d-%b-%Y')
+    since = datetime.datetime.date(
+        datetime.datetime.now() - datetime.timedelta(days=1)
+    ).strftime("%d-%b-%Y")
 
     check = check_mailbox(server, LABEL)
     if check:
         server.select(LABEL)
-        result, data = server.uid('search', None, f'(SINCE {since})')
+        result, data = server.uid("search", None, f"(SINCE {since})")
         id_list = data[0].split()
         email_rev = reversed(id_list)
         email_list = list(email_rev)
     else:
         # return Fail slack message
         pass
-    
-    fields = [
-                {
-                    "type": "mrkdwn",
-                    "text": "*email*" 
-                }
-            ]
-    if result=="OK":
-        
+
+    fields = [{"type": "mrkdwn", "text": "*email*"}]
+    if result == "OK":
+
         message = ""
         summary = ""
         for idx, num in enumerate(email_list):
             mail_text = get_mail(num, genai)
-            fields.append({"type": "mrkdwn","text": f"{mail_text}"})
+            fields.append({"type": "mrkdwn", "text": f"{mail_text}"})
             summary += f"""{idx}. {mail_text}\n"""
             if idx == 7:
                 break
-        
-        fields.append({"type": "divider"})
-        fields.append({"type": "section","text": {"type": "mrkdwn","text": genai_transform(summary, genai, prompt="아래 이메일을 요약하고 한국어로 번역 :")}})
-        
+
         temp = {
-              "text": "Unread Email",
-              "blocks": [
+            "text": "Unread Email",
+            "blocks": [
+                {"type": "section", "fields": fields},
+                {"type": "divider"},
                 {
-                  "type": "section",
-                  "fields": fields
-                }
-              ]
-            }
-    
-        with open('send_email.json', 'w') as outfile:
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": genai_transform(
+                            summary,
+                            genai,
+                            prompt="아래 이메일을 요약하고 한국어로 번역 :",
+                        ),
+                    },
+                },
+            ],
+        }
+
+        with open("send_email.json", "w") as outfile:
             json.dump(temp, outfile, indent=4)
-    
-    
